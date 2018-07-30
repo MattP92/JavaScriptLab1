9a772b8e4fd761891267cc899a325961a89f1fdf
MattP92 <39284208+MattP92@users.noreply.github.com>
2018-07-30 19:56:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const name1 = prompt("Enter a name");
 const name2 = prompt("Enter another name"); 
 const name3 = prompt("Enter a third name"); 
 
-if(name3.length == name2.length){
+if(name3.length == name2.length, name1.length){
+    console.log(`${name3} and ${name2} and ${name1} are the sames length`);
+    
+}else if(name3.length == name2.length){
     console.log(`${name3} and ${name2} are the same length`);
     
 }else if(name3.length == name1.length){
